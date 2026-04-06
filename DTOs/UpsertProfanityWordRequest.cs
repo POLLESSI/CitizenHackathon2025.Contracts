72c5250f16c0680d84ba6b5d5746c47b5f7fbf61
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-04-06 18:12:43 +0200

    Version 06 04 2026 Instauration de l'autocensure via ProfanityWord et intégration d'un script dédié.

---
+++ DTOs/UpsertProfanityWordRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CitizenHackathon2025.Contracts.DTOs
+{
+    public sealed class UpsertProfanityWordRequest
+    {
+        public string Word { get; set; } = "";
+        public string LanguageCode { get; set; } = "fr";
+        public int Weight { get; set; } = 10;
+        public bool IsRegex { get; set; }
+        public string? Category { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
---
+++ Hubs/ModerationHubMethods.cs
@@ -0,0 +1,7 @@
+﻿namespace CitizenHackathon2025.Contracts.Hubs
+{
+    public static class ModerationHubMethods
+    {
+        public const string HubPath = "moderationHub";
+    }
+}
