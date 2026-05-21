882fefdf8d0e1d4cf72796c6e6769c6aaec9ea9d
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-05-21 20:37:59 +0200

    Vdersion 21 05 2026 Amélioration de la cybersécurité vis à vis des providers + Register et Login

---
+++ Enums/WeatherProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace CitizenHackathon2025.Contracts.Enums
+{
+    public enum WeatherProvider
+    {
+        Unknown = 0,
+        OpenWeather = 1,
+        Generated = 2,
+        Manual = 3,
+        Seed = 4
+    }
+}
