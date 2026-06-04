2a601963b59d4c36243492ae314695dfb1980e5f
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-06-04 13:21:03 +0200

    Version 04 06 2026 Amélioration de CrowdInfo pour la FULL ALERT à déclencher manuellement par l'action de l'utilisateur

---
+++ DTOs/FullAlertDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CitizenHackathon2025.Contracts.DTOs
+{
+    public class FullAlertDTO
+    {
+        public int PlaceId { get; set; }
+        public string PlaceName { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime DeclaredAtUtc { get; set; }
+        public DateTime ExpiresAtUtc { get; set; }
+    }
+}
