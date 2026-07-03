22a8d6967891d5c7dcfa86f99431a2492f7c870f
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-07-03 20:58:15 +0200

    Version 03 07 2026 Instoration de la fusion des alertes et implémentation d'un commandCenter pour rendre l'application beaucoup plus décisionnelle.

---
+++ DTOs/CrowdAlertCluster.cs
@@ -0,0 +1,174 @@
+﻿namespace CitizenHackathon2025.Contracts.DTOs
+{
+    public sealed class CrowdAlertCluster
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string ZoneName { get; set; } = "Unknown zone";
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public byte Severity { get; set; }
+
+        public int RiskScore { get; set; }
+
+        public int TotalActiveConnections { get; set; }
+        public int TotalUniqueDevices { get; set; }
+
+        public int EstimatedPopulation { get; set; }
+
+        public int AntennaCount { get; set; }
+
+        public List<int> AntennaIds { get; set; } = new();
+        public List<long> AlertIds { get; set; } = new();
+
+        public DateTime FirstDetectedAtUtc { get; set; }
+        public DateTime LastDetectedAtUtc { get; set; }
+
+        public string Status { get; set; } = "PendingValidation";
+
+        public string Message { get; set; } = "";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Copyrigtht (c) 2025 Citizen Hackathon https://github.com/POLLESSI/Citizenhackathon2025.API. All rights reserved.
