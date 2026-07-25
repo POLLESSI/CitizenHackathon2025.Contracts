3cc94e38b8f55413d1778b34394a1e133d4105b8
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-07-25 22:34:16 +0200

    version 25 07 2026 Elaboration d'une charte pour éviter les débordement en cas d'événement Hugging Face

---
+++ Enums/OutZenSearchPurpose.cs
@@ -0,0 +1,15 @@
+﻿namespace CitizenHackathon2025.Contracts.Enums
+{
+    public enum OutZenSearchPurpose
+    {
+        Tourism,
+        Culture,
+        Heritage,
+        Nature,
+        Event,
+        Weather,
+        Traffic,
+        Restaurant,
+        LocalBusiness
+    }
+}
