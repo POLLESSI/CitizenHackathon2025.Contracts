055bb6ed76b022930a6b4252bc25af43c8725bfa
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-07-27 17:00:46 +0200

    Version 27 07 2026 Poursuite des adaptations pour la préventions d'événements Hugging Face

---
+++ DTOs/LocalCrowdDecisionRequest.cs
@@ -0,0 +1,155 @@
+﻿namespace CitizenHackathon2025.Contracts.DTOs
+{
+    public sealed class LocalCrowdDecisionRequest
+    {
+        public string ZoneName { get; set; } = "";
+        public int ActiveConnections { get; set; }
+        public int UniqueDevices { get; set; }
+        public int? BaselineConnections { get; set; }
+        public int RiskScore { get; set; }
+        public byte Severity { get; set; }
+        public bool HasWeatherRisk { get; set; }
+        public bool HasTrafficRisk { get; set; }
+        public bool HasKnownEvent { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Copyrigtht (c) 2025 Citizen Hackathon https://github.com/POLLESSI/Citizenhackathon2025.API. All rights reserved.
