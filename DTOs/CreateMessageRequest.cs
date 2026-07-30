49dc80cc20d02fa69aa00f0765698ea42a2804f2
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-07-30 20:32:05 +0200

    Version 30 07 2026 Effacement de CreateMessageRequest.cs de l'API car elle faisait conflit avec celle dans Contracts un peu de nettoyage dans l'architecture et amélioration de MessageCotroller.cs pour les intéractions de l'utilisateur.

--- DTOs/CreateMessageRequest.cs
+++ DTOs/CreateMessageRequest.cs
@@ -1,13 +1,19 @@
-﻿namespace CitizenHackathon2025.Contracts.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitizenHackathon2025.Contracts.DTOs
 {
     public sealed class CreateMessageRequest
     {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(2_000)]
         public string Content { get; set; } = string.Empty;
 
+        [MaxLength(32)]
         public string SourceType { get; set; } = "Other";
 
         public int? RelatedId { get; set; }
-
+        [MaxLength(256)]
         public string? RelatedName { get; set; }
 
         public double? Latitude { get; set; }
